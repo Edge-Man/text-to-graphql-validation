7273d8f11d4aaa546119ef609562123c0b95636c
Andre Carrera <acarrera94@gmail.com>
2020-05-19 15:52:42 -0600

    added calculation for accuracy

--- validation/exact_match.py
+++ validation/exact_match.py
@@ -1,9 +1,5 @@
 from graphql import parse
 
-# This is exact set matching? it's either correct or not.
-# "Full set matching" would be comparing the name of each node in the tree. it would be an accuracy out of the number of nodes.
-# so i would be comparing the child nodes at each level to get a percentage correct.
-
 
 # returns true if the prediction matches the target where any arguments or selection_sets are in unordered sets
 # prediction and target are both string graphql queries
@@ -12,3 +8,10 @@ def exact_set_match(prediction, target):
     target_document = parse(target)
     return prediction_document == target_document
 
+
+# calculates exact match accuracy over a list of predictions and their corresponding targets
+def exact_match_accuracy(predictions, targets):
+    assert len(predictions) == len(targets)
+    matches = [1 if exact_set_match(prediction, target) else 0 for prediction, target in zip(predictions, targets)]
+    sum(matches)
+    return sum(matches)/len(targets)
