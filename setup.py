2afb8d14b8d2a11faf7d7c06625847293d71fe28
Andre Carrera <acarrera94@gmail.com>
2020-05-19 16:39:54 -0600

    install deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="graphqlval", # Replace with your own username
+    name="graphqlval",  # Replace with your own username
     version="0.0.1",
     author="Andre Carrera",
     description="A small example package",
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/acarrera94/text-to-graphql-validation",
     packages=setuptools.find_packages(),
-    dependency_links=['git+https://github.com/acarrera94/graphql-core@37044c820ffe505e26a7c84a66576ee8d2690e1e#egg=graphql-core'],
+    install_requires=['graphql-core'],
+    dependency_links=[
+        'git+https://github.com/acarrera94/graphql-core@handle_sets#egg=graphql-core'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
