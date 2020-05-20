2a0271ecbcfea7c137740de1f116e9f4291c4518
Andre Carrera <acarrera94@gmail.com>
2020-05-20 11:46:44 -0600

    handling of errors

--- graphqlval/exact_match.py
+++ graphqlval/exact_match.py
@@ -3,15 +3,24 @@ from graphql import parse
 
 # returns true if the prediction matches the target where any arguments or selection_sets are in unordered sets
 # prediction and target are both string graphql queries
-def exact_set_match(prediction, target):
+def is_exact_set_match(prediction, target):
     prediction_document = parse(prediction, no_location=True)
     target_document = parse(target, no_location=True)
     return prediction_document == target_document
 
 
+# calls is_exact_set_match.
+# 1 for true 0 for false
+# handles errors as 0
+def exact_set_match(prediction, target):
+    try:
+        return int(is_exact_set_match(prediction, target) == True)
+    except:
+        return 0
+
+
 # calculates exact match accuracy over a list of predictions and their corresponding targets
 def exact_match_accuracy(predictions, targets):
     assert len(predictions) == len(targets)
-    matches = [1 if exact_set_match(prediction, target) else 0 for prediction, target in zip(predictions, targets)]
-    sum(matches)
-    return sum(matches)/len(targets)
+    matches = [exact_set_match(prediction, target) for prediction, target in zip(predictions, targets)]
+    return sum(matches) / len(targets)
